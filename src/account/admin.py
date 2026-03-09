b6d346b8453ecc7bae8a15c49fcfa14dc11898b3
ngothikimoanh <ntkoanh.orin@outlook.com>
2026-03-09 20:43:05 +0700

    add admin page for manage user

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from typing import override
 
-# Register your models here.
+from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+
+from account.models.user import User
+
+
+@admin.register(User)
+class AdminManager(admin.ModelAdmin):
+    list_display = ["email", "email_verified", "phone_number", "phone_number_verified", "gender", "birthday", "address"]
+
+    @override
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.exclude(is_superuser=True)
