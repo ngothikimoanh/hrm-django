a1c7054bc403b949957d106f801017a5c6fd7b72
ngothikimoanh <ntkoanh.orin@outlook.com>
2026-03-24 10:04:23 +0700

    fix: normalize email & check duplicate

--- src/account/forms/user/register.py
+++ src/account/forms/user/register.py
@@ -3,6 +3,7 @@ from typing import override
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.forms import ValidationError
 
 User = get_user_model()
 
@@ -14,7 +15,14 @@ class UserRegisterForm(forms.ModelForm):
         model = User
         fields = ["email", "password"]
 
+    def clean_email(self):
+        email = self.cleaned_data["email"].strip().lower()
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email is exist.")
+        return email
+
     @override
     def save(self):
+
         user = User.objects.create_user(email=self.cleaned_data["email"], password=self.cleaned_data["password"])
         return user
