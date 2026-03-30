1de3d2381eef019fb8e33fd8d46f05f98f716880
phamquiduong <phamquiduong@outlook.com>
2026-03-30 23:47:02 +0700

    fix logic verify email

--- src/account/services/email_verify/link_verify.py
+++ src/account/services/email_verify/link_verify.py
@@ -1,9 +1,12 @@
+import logging
 from typing import override
 
 from django.utils import timezone
 
 from account.models.verify_email import EmailVerifyToken
 from account.services.email_verify.base import EmailVerify
+
+logger = logging.getLogger()
 
 
 class LinkVerifier(EmailVerify):
@@ -17,12 +20,15 @@ class LinkVerifier(EmailVerify):
 
     @override
     def is_valid(self) -> bool:
+        logger.info("Start valid email token [%s]", self.token)
         self.email_verify_token = EmailVerifyToken.objects.filter(token=self.token).first()
 
         if self.email_verify_token is None:
+            logger.error("Token does not exist")
             return False
 
-        if self.email_verify_token.expired_at > timezone.now():
+        if self.email_verify_token.expired_at < timezone.now():
+            logger.error("Token is expired")
             return False
 
         self.user = self.email_verify_token.user
