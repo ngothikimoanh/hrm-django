8f8d23730dbc9ea4da2c306040951b168b4415bd
ngothikimoanh <ntkoanh.orin@outlook.com>
2026-04-14 00:31:40 +0700

    send otp to verify email

--- src/account/services/email_verify/otp_verify.py
+++ src/account/services/email_verify/otp_verify.py
@@ -1,11 +1,11 @@
 import logging
 from typing import override
 
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 from account.models.verify_email import EmailVerifyOTP
 from account.services.email_verify.base import EmailVerify
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -13,7 +13,7 @@ logger = logging.getLogger()
 
 
 class OTPVerifier(EmailVerify):
-    def __init__(self, otp: str| None, user: User):
+    def __init__(self, otp: str | None, user: User):
         self.user = user
         self.otp = otp
         self.email_verify_otp: EmailVerifyOTP | None = None
@@ -24,16 +24,29 @@ class OTPVerifier(EmailVerify):
 
     @override
     def is_valid(self) -> bool:
-        logger.info("Start valid email otp [%s]", self.otp)
-        self.email_verify_otp = EmailVerifyOTP.objects.filter(user=self.user,otp=self.otp).first()
+        logger.info("Start valid email OTP [%s]", self.otp)
+
+        self.email_verify_otp = EmailVerifyOTP.objects.filter(otp=self.otp).first()
 
         if self.email_verify_otp is None:
-            logger.error("Otp does not exist")
+            logger.error("OTP does not exist")
             return False
 
         if self.email_verify_otp.expired_at < timezone.now():
-            logger.error("Otp is expired")
+            logger.error("OTP is expired")
             return False
 
         self.user = self.email_verify_otp.user
+        return True
+
+    def verify(self) -> bool:
+        if not self.email_verify_otp:
+            logger.error("OTP not validated before verify")
+            return False
+
+        self.email_verify_otp.delete()
+
+        self.user.email_verified = True
+        self.user.save()
+
         return True
--- src/account/views/user/verify_email.py
+++ src/account/views/user/verify_email.py
@@ -1,54 +1,50 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.http import HttpRequest, HttpResponseBadRequest
-from django.shortcuts import redirect
-from django.views.decorators.http import require_GET
+from django.shortcuts import redirect, render
 
 from account.models.verify_email import EmailVerifyOTP
 from account.services.email.verify_email import OTPVerifyEmail
-from account.services.email_verify.link_verify import LinkVerifier
-
-from django.shortcuts import render
-
 from account.services.email_verify.otp_verify import OTPVerifier
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-@require_GET
-def verify_link_email_view(request: HttpRequest):
-    service = LinkVerifier(token=request.GET["token"])
-    service.clean()
+# @require_GET
+# def verify_link_email_view(request: HttpRequest):
+#     service = LinkVerifier(token=request.GET["token"])
+#     service.clean()
 
-    if service.is_valid():
-        service.save()
-        messages.success(request, "Email verified successfully!")
-        return redirect("account-home")
+#     if service.is_valid():
+#         service.save()
+#         messages.success(request, "Email verified successfully!")
+#         return redirect("account-home")
 
-    return HttpResponseBadRequest("Invalid token")
+#     return HttpResponseBadRequest("Invalid token")
 
 
-def otp_view(request: HttpRequest):
-    if request.method != "POST":
-        return redirect("otp")
-
-    EmailVerifyOTP.objects.filter(user=request.user).delete()
-
+def send_otp_view(request: HttpRequest):
     email_verify_otp = EmailVerifyOTP.objects.create(user=request.user)
     email_service = OTPVerifyEmail()
     html_content = email_service.generate_html({"email_verify_otp": email_verify_otp}, request=request)
-    email_service.send_mail(to_emails=[request.user.email],html_content=html_content)
+    email_service.send_mail(to_emails=[request.user.email], html_content=html_content)
 
     return render(request, "account/otp_verify.html")
 
 
 def verify_otp_email_view(request: HttpRequest):
     service = OTPVerifier(user=request.user, otp=request.POST.get("otp"))
-    service.clean()
 
-    if service.is_valid():
-        service.save()
-        messages.success(request, "Email verified successfully!")
-        return redirect("account-home")
+    try:
+        service.clean()
+    except ValueError as e:
+        return HttpResponseBadRequest(str(e))
 
-    return HttpResponseBadRequest("Invalid token")
+    if not service.is_valid():
+        return HttpResponseBadRequest("OTP invalid")
+
+    if not service.verify():
+        return HttpResponseBadRequest("Verify fail")
+
+    messages.success(request, "Email verified successfully!")
+    return redirect("account-home")
